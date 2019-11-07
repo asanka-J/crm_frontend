88c36084e7b33bf694a2bff535e374bb7e16e6a3
asanka94 <asanka.jsndr@gmail.com>
2019-11-07 23:47:49 +0530

    User module files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/tailwind.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import Register from "./Register";
+import GuestRoute from "./components/GuestRoute";
+import AuthRoute from "./components/AuthRoute";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <div className="bg-gray-300 h-screen">
+          <GuestRoute path="/login" component={Login} />
+          <GuestRoute path="/register" component={Register} />
+          <AuthRoute path="/profile" component={Profile} />
+        </div>
+      </Layout>
+    </Router>
   );
 }
 
